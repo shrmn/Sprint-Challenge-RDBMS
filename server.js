1207dcbf4beaad4561078ef709619d83a84e4305
Dan Sherman <dvsherman@protonmail.com>
2019-03-29 08:42:46 -0700

    ran knex init, set up knexfile.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-server.use('/api/projects', projectRouter);
-server.use('/api/actions', actionRouter);
+// server.use('/api/projects', projectRouter);
+// server.use('/api/actions', actionRouter);
 
 server.get('/', (req, res) => {
   res.send(`
